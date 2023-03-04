51ab18cf63bd7472f6e7a60d766c846a6ae1b385
numan ahmed <numanahmedmail@gmail.com>
2023-03-04 16:47:08 +0500

    portfolio completed

--- js/index.js
+++ js/index.js
@@ -3,10 +3,11 @@ let menuIcon = document.querySelector('#menu_icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () =>{
-    console.log('hi');
+    // console.log('hi');
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-}
+};
+
 // ================== scroll section active link ==================
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -28,4 +29,34 @@ window.onscroll = () =>{
     // ================== sticky navBar ==================
     let header = document.querySelector('header');
     header.classList.toggle('sticky',window.scrollY > 100);
+
+
+    // ================== remove toggle icon and navbar when click navbar link (scroll) ==================
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+    
+    
 };
+
+// ================== scroll reveal ==================
+
+ScrollReveal({
+    // reset: true, // true -> animation every time
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading',{ origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form',{ origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1 , .about-img ',{ origin: 'left'});
+ScrollReveal().reveal('.home-content p , .about-content ',{ origin: 'right'});
+
+// ================== typed js ==================
+const typed = new Typed('.multiple-text', {
+    string : ['Frontend Developer','Flutter Developer', 'UI/UX Designer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+ });
